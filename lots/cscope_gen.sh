78dfec1c725703e09b9a2cad5bc2fd228657d674
cosmin <cosmin@work-machine.rdsnet.ro>
2014-03-08 01:38:56 +0200

    New Files

---
+++ lots/cscope_gen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+find . - name '*.py' \
+    -o -name '*.java' \
+    -o -iname '*.[CH]' \
+    -o -name '*.cpp' \
+    -o -name '*.cc' \
+    -o -name '*.hpp' \
+    > cscope.files
+# -b: just build
+# -q: create inverted index
+cscope -b -q

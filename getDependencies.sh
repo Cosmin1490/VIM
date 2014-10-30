f20bec0e854e9397249eb4a662d029817ee9cc3f
Radu-Cosmin Balan <cbalan@cbalan-mbp.local>
2014-10-30 14:27:44 -0700

    configs

---
+++ getDependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install ctags
+brew install vim
+
--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ svn co svn://gcc.gnu.org/svn/gcc/trunk/libstdc++-v3/python
 cd ..
 cp config/gdbinit ../.gdbinit
 cp config/ctags ../.ctags
+cat ../.bash_profile config/bash_profile >../.bash_profile

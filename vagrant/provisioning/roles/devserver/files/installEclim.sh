205f7a3e22c9bd26087d44d79a9094ccb8300926
Radu-Cosmin Balan <bcosmin1411@yahoo.com>
2015-08-17 00:13:02 +0100

    vagrant update

--- vagrant/provisioning/roles/devserver/files/installEclim.sh
+++ vagrant/provisioning/roles/devserver/files/installEclim.sh
@@ -2,20 +2,20 @@
 
 Xvfb :1 -screen 0 1024x768x24 &
 
-DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
-  -application org.eclipse.equinox.p2.director \
-  -repository http://download.eclipse.org/releases/luna \
-  -installIU org.eclipse.wst.web_ui.feature.feature.group
-
-DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
-  -application org.eclipse.equinox.p2.director \
-  -repository http://download.eclipse.org/releases/luna \
-  -installIU org.eclipse.jdt.feature.group
-
-DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
-  -application org.eclipse.equinox.p2.director \
-  -repository http://download.scala-ide.org/sdk/lithium/e44/scala211/dev/site \
-  -installIU org.scala-ide.sdt.feature.feature.group
+#DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
+#  -application org.eclipse.equinox.p2.director \
+#  -repository http://download.eclipse.org/releases/luna \
+#  -installIU org.eclipse.wst.web_ui.feature.feature.group
+#
+#DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
+#  -application org.eclipse.equinox.p2.director \
+#  -repository http://download.eclipse.org/releases/luna \
+#  -installIU org.eclipse.jdt.feature.group
+#
+#DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
+#  -application org.eclipse.equinox.p2.director \
+#  -repository http://download.scala-ide.org/sdk/lithium/e44/scala211/dev/site \
+#  -installIU org.scala-ide.sdt.feature.feature.group
 
 #java \
 #  -Dvim.skip=true \

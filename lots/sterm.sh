94791fa82b73ba5d4fee210c2b3e69ac9f80a304
Radu-Cosmin Balan <bcosmin1411@yahoo.com>
2015-08-16 20:41:05 +0100

    Sterm whitespace

--- lots/sterm.sh
+++ lots/sterm.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 osascript -e "
-tell application \"iTerm\" 
-    activate 
-    set myterm to (make new terminal) 
-    tell myterm 
+tell application \"iTerm\"
+    activate
+    set myterm to (make new terminal)
+    tell myterm
 
 	launch session \"Default Session\"
-        tell application \"System Events\" to keystroke \"d\" using command down 
+    tell application \"System Events\" to keystroke \"d\" using command down
 	launch session \"Default Session\"
 
 	tell application \"System Events\" to key code 126 using command down
 
 	tell item 1 in sessions
-       		set name to \"LeftPane\"
-                write text \"ssh private\"
+    set name to \"LeftPane\"
+    write text \"ssh private\"
 	end tell
-	
+
 	tell item 2 in sessions
-		set name to \"RightPane\"
-        	write text \"ssh private\"
+    set name to \"RightPane\"
+    write text \"ssh private\"
 	end tell
 
 	tell item 3 in sessions
-       		set name to \"VVIM\"
-                write text \"ssh private\"
-	end tell	
+    set name to \"VVIM\"
+    write text \"ssh private\"
+	end tell
 
 	delay 3
 
@@ -44,7 +44,7 @@ tell application \"iTerm\"
 		write text \"$3\"
 		write text \"screen -d -R vvim\"
 		delay 1
-	end tell	
-    end tell 
+	end tell
+    end tell
 
 end tell"

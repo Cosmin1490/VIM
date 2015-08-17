3adfd52aa6dd324e0d29fc04b7382d42b3113b61
Radu-Cosmin Balan <bcosmin1411@yahoo.com>
2015-08-17 02:26:55 +0100

    update vagrant configs

--- vagrant/provisioning/roles/devserver/files/installEclim.sh
+++ vagrant/provisioning/roles/devserver/files/installEclim.sh
@@ -2,24 +2,19 @@
 
 Xvfb :1 -screen 0 1024x768x24 &
 
-#DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
-#  -application org.eclipse.equinox.p2.director \
-#  -repository http://download.eclipse.org/releases/luna \
-#  -installIU org.eclipse.wst.web_ui.feature.feature.group
-#
-#DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
-#  -application org.eclipse.equinox.p2.director \
-#  -repository http://download.eclipse.org/releases/luna \
-#  -installIU org.eclipse.jdt.feature.group
-#
-#DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
-#  -application org.eclipse.equinox.p2.director \
-#  -repository http://download.scala-ide.org/sdk/lithium/e44/scala211/dev/site \
-#  -installIU org.scala-ide.sdt.feature.feature.group
+DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
+    -application org.eclipse.equinox.p2.director \
+    -repository http://download.eclipse.org/releases/mars/ \
+    -installIU org.eclipse.wst.web_ui.feature.feature.group
 
-#java \
-#  -Dvim.skip=true \
-#  -Declipse.home=/home/vagrant/eclipse \
-#  -jar eclim_2.5.5.jar install
+DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
+    -application org.eclipse.equinox.p2.director \
+    -repository http://dist.springsource.org/snapshot/GRECLIPSE/e4.5/ \
+    -installIU org.codehaus.groovy.eclipse.feature.feature.group
+
+DISPLAY=:1 ./eclipse/eclipse -nosplash -consolelog -debug \
+    -application org.eclipse.equinox.p2.director \
+    -repository http://download.scala-ide.org/sdk/lithium/e44/scala211/stable/site \
+    -installIU org.scala-ide.sdt.feature.feature.group
 
 #DISPLAY=:1 ./eclipse/eclimd -b

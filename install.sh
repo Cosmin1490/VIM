617ed77b8ddac8d09ff0be1a90e841aa756ad7fd
Radu-Cosmin Balan <bcosmin1411@yahoo.com>
2016-06-05 01:14:22 +0100

    remove vimperator from install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ svn co svn://gcc.gnu.org/svn/gcc/trunk/libstdc++-v3/python
 cd ..
 cp config/gdbinit ../.gdbinit
 cp config/ctags ../.ctags
-cp config/vimperatorrc ../.vimperatorrc
 cat ../.bash_profile config/bash_profile >>../.bash_profile

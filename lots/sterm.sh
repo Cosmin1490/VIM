0de516db3203fa6df8d684b33e213be76251db9b
Radu-Cosmin Balan <rbalan@palantir.com>
2016-10-28 16:31:38 +0100

    update sterm

--- lots/sterm.sh
+++ lots/sterm.sh
@@ -2,69 +2,43 @@
 osascript -e "
 tell application \"iTerm\"
   activate
-  set myterm to (make new terminal)
+  set myterm to (create window with default profile)
+
   tell myterm
 
     tell application \"System Events\" to key code 126 using command down
 
-    launch session \"Default Session\"
-    delay 0.1
-
-    launch session \"Default Session\"
-    tell application \"System Events\" to keystroke \"d\" using command down
-    tell application \"System Events\" to keystroke \"d\" using command down
-    delay 0.1
-
-    launch session \"Default Session\"
-    delay 0.1
-
-    tell item 1 in sessions
+    tell current session of myterm
       set name to \"UI Vim\"
-    end tell
-
-    tell item 2 in sessions
-      set name to \"Scripts\"
-    end tell
-
-    tell item 3 in sessions
-      set name to \"Server\"
-    end tell
-
-    tell item 4 in sessions
-      set name to \"Grunt\"
-    end tell
-
-    tell item 5 in sessions
-      set name to \"Git\"
-    end tell
-
-    delay 1
-
-    tell item 1 in sessions
-      activate
-      write text \"cd ~/code/projectxxx/projectxxx-web-app\"
+      write text \"cd ~/code/lime\"
       write text \"vvim\"
       delay 0.1
     end tell
 
-    tell item 2 in sessions
-      write text \"cd ~/code/projectxxx/projectxxx-web-server\"
-      delay 0.1
+    create tab with default profile
+
+    tell current session of myterm
+      set name to \"Scripts\"
+      write text \"cd ~/code/lime\"
+      select (split vertically with default profile)
     end tell
 
-    tell item 3 in sessions
-      write text \"cd ~/code/projectxxx\"
-      delay 0.1
+    tell current session of myterm
+      set name to \"Server\"
+      write text \"cd ~/code/lime\"
+      select (split vertically with default profile)
     end tell
 
-    tell item 4 in sessions
-      write text \"cd ~/code/projectxxx/projectxxx-web-app\"
-      delay 0.1
+    tell current session of myterm
+      set name to \"Grunt\"
+      write text \"cd ~/code/lime\"
     end tell
 
-    tell item 5 in sessions
-      write text \"cd ~/code/projectxxx\"
-      delay 0.1
+    create tab with default profile
+
+    tell current session of myterm
+      set name to \"Git\"
+      write text \"cd ~/code/lime\"
     end tell
 
   end tell
